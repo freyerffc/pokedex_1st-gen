400dd093e82f657e86ea737fa8dfa1597d7bb8ac
freyerf <freyerf.fc@gmail.com>
2025-02-12 12:47:16 -0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:pokemon_app/detail_page.dart';
+import 'dart:developer';
 
 void main() {
   runApp(const MyApp());
@@ -40,8 +41,12 @@ class LandingPageState extends State<LandingPage> with SingleTickerProviderState
       vsync: this,
     )..forward();
 
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
-    _slideAnimation = Tween<Offset>(begin: const Offset(0.0, -1.0), end: Offset.zero).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+    _slideAnimation = Tween<Offset>(begin: const Offset(0.0, -1.0), end: Offset.zero).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
 
     Future.delayed(const Duration(seconds: 3), () {
       if (mounted) {
@@ -103,10 +108,11 @@ class HomePage extends StatefulWidget {
   @override
   HomePageState createState() => HomePageState();
 }
+
 class HomePageState extends State<HomePage> {
   List pokemons = [];
   bool isLoading = false;
-  bool hasMore = true;  // Añadimos una variable para controlar si hay más Pokémon por cargar
+  bool hasMore = true;
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -120,20 +126,26 @@ class HomePageState extends State<HomePage> {
       isLoading = true;
     });
 
-    // URL para obtener solo los primeros 151 Pokémon (Primera Generación)
-    var url = Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=151');
-    var response = await http.get(url);
-    
-    if (response.statusCode == 200) {
-      var data = json.decode(response.body);
+    try {
+      var url = Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=151');
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        setState(() {
+          pokemons = data['results'];
+        });
+      } else {
+        throw Exception("Error al obtener los datos");
+      }
+    } catch (e) {
+      log("Error al obtener los datos: $e");
+    } finally {
       setState(() {
-        pokemons = data['results'];
+        isLoading = false;
+        hasMore = false;
       });
     }
-    setState(() {
-      isLoading = false;
-      hasMore = false; // No hay más Pokémon por cargar
-    });
   }
 
   @override
@@ -155,13 +167,11 @@ class HomePageState extends State<HomePage> {
           controller: _scrollController,
           itemCount: pokemons.length,
           itemBuilder: (context, index) {
-            // Condición para evitar que se cargue más allá de los 151 Pokémon
-            if (index == pokemons.length - 1 && hasMore) {
-              return const Center(child: CircularProgressIndicator());
+            if (index >= pokemons.length) {
+              return hasMore ? const Center(child: CircularProgressIndicator()) : Container();
             }
-
-            var pokemonUrl = pokemons[index]['url'];
-            var pokemonNumber = pokemonUrl.split('/')[6];
+            var pokemon = pokemons[index];
+            var pokemonNumber = pokemon['url'].split('/')[6];
             var imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$pokemonNumber.png';
 
             return Card(
@@ -169,22 +179,17 @@ class HomePageState extends State<HomePage> {
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
               margin: const EdgeInsets.all(10),
               child: ListTile(
-                leading: Image.network( // Agregamos la imagen oficial
-                  imageUrl,
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.contain,
-                ),
-                title: Text(
-                  '#$pokemonNumber - ${pokemons[index]['name'].toUpperCase()}',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
+                leading: Image.network(imageUrl, width: 80, height: 80, fit: BoxFit.contain),
+                title: Text('#$pokemonNumber - ${pokemon['name'].toUpperCase()}',
+                    textAlign: TextAlign.center, style: const TextStyle(fontWeight: FontWeight.bold)),
                 onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailPage(url: pokemons[index]['url']),
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) => DetailPage(url: pokemon['url']),
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        return FadeTransition(opacity: animation, child: child);
+                      },
                     ),
                   );
                 },
@@ -192,6 +197,17 @@ class HomePageState extends State<HomePage> {
             );
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _scrollController.animateTo(
+            0,
+            duration: const Duration(seconds: 1),
+            curve: Curves.easeOut,
+          );
+        },
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.arrow_upward, color: Colors.white),
       ),
     );
   }
